8ee887a65092d5753df405c071667c461dc27f28
dynamic-chain-5255 <ritiguda@gmail.com>
2021-01-12 21:05:27 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,4 +1,13 @@
 // write your code below!
 function happyHolidays(){
-  return "Happy Holidays!"
+  return "Happy Holidays !"
+}
+function happyHolidaysTo(name){
+  return `Happy Holidays, ${name}!`
+}
+function happyCustomHolidayTo(holiday , name){
+  return `Happy ${holiday}, ${name}!`
+}
+function holidayCountdown (holiday, days){
+  return `It's ${days} days until ${holiday}!`
 }
